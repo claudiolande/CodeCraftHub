d319b405936d7f29bc2040139213914bf9e458f9
Claudio Lande <claudio.lande@aqualogybs.net>
2024-03-10 18:20:35 +0100

    Adding temporary changes to Github

---
+++ src/services/userService.js
@@ -0,0 +1,11 @@
+const User = require('../models/userModel');
+
+const createUser = async (userData) => {
+    // Add logic to create a new user in the database
+};
+
+// Add more service functions as needed
+
+module.exports = {
+    createUser,
+};
